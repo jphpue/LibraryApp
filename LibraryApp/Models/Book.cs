93b82874bf70c64fccf2bcc50b2e333ac11f4a64
jphpue <otto.kyllonen94@gmail.com>
2021-06-10 14:12:23 +0300

    added book context and a book class to back end

---
+++ LibraryApp/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Models
+{
+    public class Book
+    {
+        public string author { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+    }
+}
